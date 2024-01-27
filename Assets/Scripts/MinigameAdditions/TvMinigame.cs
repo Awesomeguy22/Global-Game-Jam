9cb70124f0fd5ce92baad795269a96396afa90b6
Oliver Long <longoliver2005@gmail.com>
2024-01-27 14:43:03 -0500

    Created minigame base class

--- Assets/Scripts/MinigameAdditions/TvMinigame.cs
+++ Assets/Scripts/MinigameAdditions/TvMinigame.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TvMinigame : MonoBehaviour
 {
+    Interactable interactable;
+    [SerializeField] float timer = 5.0f;
+    [SerializeField] float maintainChannelTime = 0.3f;
+
+    [SerializeField] float staticTransitionTime = 0.5f;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
@@ -13,6 +20,20 @@ public class TvMinigame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= 5.0f;
+
+        if (interactable.playingMinigame){
+            
+        }
     }
+
+    void StartMinigame(){
+
+    }
+
+    void ChangeChannel(){
+
+    }
+
+    
 }
