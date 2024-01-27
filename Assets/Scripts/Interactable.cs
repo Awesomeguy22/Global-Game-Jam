048f96dd79f4acc0d84398993634c836a4850f4c
Oliver Long <longoliver2005@gmail.com>
2024-01-26 20:42:38 -0500

    pushin'

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] GameObject buttonprompt;
+    
+    bool playerIsNear = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        buttonprompt.SetActive(playerIsNear);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player")) {
+            playerIsNear = true;
+
+        }
+    } 
+
+    void OnTriggerExit2D(Collider2D collision){
+        if (collision.gameObject.tag.Equals("Player")) {
+            playerIsNear = false;
+
+        }
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    bool isWalking = true;
+    bool isFacingRight = true;
+    public float leftBound = -5;
+    public float rightBound = 5;
+
+    //units per second
+    public float walkSpeed = 0.5f;
+
+    //Change later when proper animation
+    [SerializeField] Transform graphics;
     
     // Start is called before the first frame update
     void Start()
@@ -14,6 +25,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        HandleAnimation();
+    }
+
+    void Move(){
+        float walkDir = Input.GetAxis("Horizontal");
+        if (math.abs(walkDir) > 0.1f){
+            isFacingRight = walkDir < 0;
+        }
+
+        if(!isWalking){
+            return;
+        }
+        //past left wall and trying to walk left
+        if(transform.position.x < leftBound && walkDir < 0){
+            return;
+        }
+
+        //past right wall and trying to walk right
+        if(transform.position.x > rightBound && walkDir > 0){
+            return;
+        }
+        transform.Translate(Vector3.right * walkDir * walkSpeed / 50);
+    }
+
+    void HandleAnimation(){
+        if (isFacingRight) {
+            graphics.rotation = Quaternion.Euler(0,0,0); 
+        } else {
+            graphics.rotation = Quaternion.Euler(0,180,0);
+        }
     }
 }
