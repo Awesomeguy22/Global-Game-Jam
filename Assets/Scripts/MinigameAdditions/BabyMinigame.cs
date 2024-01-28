6d83ecd374cb87031ccd3459c08106b29f7398d6
Oliver Long <longoliver2005@gmail.com>
2024-01-27 22:03:22 -0500

    Baby Minigame Starting

--- Assets/Scripts/MinigameAdditions/BabyMinigame.cs
+++ Assets/Scripts/MinigameAdditions/BabyMinigame.cs
@@ -6,12 +6,28 @@ using UnityEngine;
 public class BabyMinigame : Minigame
 {
     int inputIndex;
-    String[] inputSequence; 
 
+    //the sequence the player must match to complete the baby game
+    int[] inputSequence; 
+
+    [SerializeField] int sequenceLength = 4;
+    
     [SerializeField] GameObject[] keys;
+    [SerializeField] Vector3[] IndicatorPositions;
+
+    int currentIndex;
+
+    [ContextMenu("ActivateMinigame")]
     public override void ActivateMinigame()
     {
+        Debug.Log("Baby Task Starting");
+        inputSequence = new int[sequenceLength];
         //generate input sequence
+        for(int i = 0; i < sequenceLength; i++){
+            inputSequence[i] = UnityEngine.Random.Range(0,4);
+            Instantiate(keys[inputSequence[i]],IndicatorPositions[i], Quaternion.identity, transform);
+        }
+        
     }
 
     public override void EndMinigame()
@@ -33,6 +49,32 @@ public class BabyMinigame : Minigame
     // Update is called once per frame
     void Update()
     {
-        
+        if(!_enabled){
+            return;
+        }
+        string currentChar = "";
+        switch(inputSequence[currentIndex]){
+            case 0:
+            currentChar = "W";
+            break;
+            case 1:
+            currentChar = "A";
+            break;
+            case 2:
+            currentChar = "S";
+            break;
+            case 3:
+            currentChar = "D";
+            break;
+        }
+
+        KeyCode currentKeyCode = (KeyCode) System.Enum.Parse(typeof(KeyCode), currentChar);
+        if(Input.GetKeyDown(currentKeyCode)){
+            Debug.Log("Hello");
+        }
+
     }
+
+
+    
 }
